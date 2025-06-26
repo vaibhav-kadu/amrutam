d6554bd20112c2bfcfddbf3e78ae92a4133b2c9a
vaibhav-kadu <vkdon02@gmail.com>
2025-06-26 10:12:15 +0530

    Implement logic for Home.java

---
+++ app/src/main/java/com/amrutamtechnology/amrutam/Home.java
@@ -0,0 +1,23 @@
+package com.amrutamtechnology.amrutam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //EdgeToEdge.enable(this);
+        setContentView(R.layout.home);
+        Button btn=findViewById(R.id.button11);
+
+    }
+}
